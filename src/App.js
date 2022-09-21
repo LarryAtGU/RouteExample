4242d93a997b4d54ae048e493cd90b64620f2873
Larry Wen <s1834299@staff.ad.griffith.edu.au>
2022-09-21 17:39:32 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/welcome";
 import Products from "./pages/products";
+import MainHeader from "./components/mainheader";
+const ProductDetail = () => {
+  return <div>Product Details</div>;
+};
+
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader></MainHeader>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/welcome" />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
+        </Route>
+      </Switch>
     </div>
   );
 }
